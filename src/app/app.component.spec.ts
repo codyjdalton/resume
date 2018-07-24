015a1b430d781fd86a9a5cf69897a4e6a11bd876
Cody Dalton <codydalton@Codys-MacBook-Pro.local>
2018-07-23 21:44:58 -0700

    Round 1: Make tests pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+
+import { ResumeModule } from './resume/resume.module';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ResumeModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -22,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Cody Dalton');
   }));
 });
